91694261fd5d29740e4b3d830fbd5f1b3c4b71a0
tinh ha <hxltinh@gmail.com>
2016-07-24 09:39:33 +0700

    update code for observable

--- src/ts/services/hero.ts
+++ src/ts/services/hero.ts
@@ -6,18 +6,20 @@ import { Injectable } from "@angular/core";
 import { Headers, Http, Response } from "@angular/http";
 import Heroes from "../mocks/hero";
 import Hero from "../interfaces/hero";
-import { Observable } from 'rxjs/Observable';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from "rxjs/Observable";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
 
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/find';
-import 'rxjs/add/observable/from';
+import { find } from "lodash";
+
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/find";
+import "rxjs/add/observable/from";
 
 
 @Injectable()
 export default class HeroService {
 
-  private heroesUrl = '/json/hero.json';
+  private heroesUrl = "/json/hero.json";
   constructor(private http: Http) {}
 
   getHeroes(): Observable<Hero[]> {
@@ -25,25 +27,8 @@ export default class HeroService {
   }
 
   getHero(id: number): Observable<Hero> {
-
-    // const heroFound = new BehaviorSubject(new Hero);
-    //
-    // this.getHeroes().subscribe((heroes:Hero[]) => {
-    //   heroFound.onNext(Observable.from(heroes).find((hero:Hero) => hero.id === id));
-    // })
-    //
-    // return heroFound;
     return this.getHeroes().map((heroes:Hero[]) => {
-      console.info('hero service:', heroes);
-      return true;
+      return find(heroes, (hero:Hero) => hero.id === id);
     });
-
-    // return this.getHeroes().subscribe((heroes:Hero[]) => {
-    //   return Observable.from(heroes).find((hero:Hero) => hero.id === id)
-    // });
   }
-  // getHero(id: number) {
-  //   return this.getHeroes()
-  //              .then(heroes => heroes.filter(hero => hero.id === id)[0]);
-  // }
 }
